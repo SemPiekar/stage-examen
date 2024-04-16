0f0b52f64160596f6ee77f7f6e46611f4200b237
Sem Piekar <9018659@student.zadkine.nl>
2024-04-16 15:11:32 +0200

    L

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate instead of useHistory
+import { useNavigate } from "react-router-dom";
 import usersData from "../../data/users.json";
 import Footer from "../footer/footer";
 import { Button } from "react-bootstrap";
@@ -7,7 +7,8 @@ import { Button } from "react-bootstrap";
 export default function Login({ onLogin }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate(); // Use useNavigate hook for navigation
+  const [showWarning, setShowWarning] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,50 +17,79 @@ export default function Login({ onLogin }) {
     );
     if (user) {
       onLogin(user);
-      navigate(`/${user.role}/dashboard`); // Use navigate function to redirect
+      navigate(`/${user.role}/dashboard`);
     } else {
-      alert("Invalid credentials");
+      setShowWarning(true);
     }
   };
 
   return (
-    <div>
-      <div className="container mt-5 col-2 d-flex flex-column justify-content-center">
-        <h2>iTeachers</h2>
-        <form onSubmit={handleSubmit}>
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">
-              @
-            </span>
-            <input
-              type="text"
-              class="form-control"
-              id="username"
-              value={username}
-              placeholder="Username"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <Button className="w-100" type="submit" variant="primary" size="lg">
-            Inloggen
-          </Button>
-        </form>
+    // <div>
+    //   <div className="container mt-5 col-2 d-flex flex-column justify-content-center">
+    //     <h2>iTeachers</h2>
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="input-group mb-3">
+    //         <span className="input-group-text" id="basic-addon1">
+    //           @
+    //         </span>
+    //         <input
+    //           type="text"
+    //           className="form-control"
+    //           id="username"
+    //           value={username}
+    //           placeholder="Username"
+    //           aria-label="Username"
+    //           aria-describedby="basic-addon1"
+    //           onChange={(e) => setUsername(e.target.value)}
+    //         ></input>
+    //       </div>
+    //       <div className="mb-3">
+    //         <label htmlFor="password" className="form-label">
+    //           Password
+    //         </label>
+    //         <input
+    //           type="password"
+    //           className="form-control"
+    //           id="password"
+    //           value={password}
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </div>
+    //       <Button className="w-100" type="submit" variant="primary" size="lg">
+    //         Inloggen
+    //       </Button>
+    //     </form>
+    //     {showWarning && (
+    //       <div className="alert alert-warning mt-3" role="alert">
+    //         Ongeldige gebruikersnaam of wachtwoord
+    //       </div>
+    //     )}
+    //   </div>
+    //   <Footer />
+    // </div>
+    <main class="form-signin col-2 m-auto vh-100 d-flex flex-column justify-content-center">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+  
+      <div class="form-floating">
+        <input type="email" class="form-control rounded-bottom-0" id="floatingInput" placeholder="name@example.com"></input>
+        <label for="floatingInput">Email address</label>
       </div>
-      <Footer />
-    </div>
+      <div class="form-floating">
+        <input type="password" class="form-control rounded-top-0" id="floatingPassword" placeholder="Password"></input>
+        <label for="floatingPassword">Password</label>
+      </div>
+  
+      <div class="form-check text-start my-3">
+        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault"></input>
+        <label class="form-check-label" for="flexCheckDefault">
+          Remember me
+        </label>
+      </div>
+      <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+    </form>
+    <Footer />
+  </main>
   );
 }
--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -2,36 +2,33 @@ import React from "react";
 
 const footer = () => {
   return (
-    <div class="container fixed-bottom">
-      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
+    <div className="container fixed-bottom">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
           <a
             href="/"
-            class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
+            className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
           >
-            <svg class="bi" width="30" height="24">
-              <use xlink:href="#bootstrap" />
-            </svg>
           </a>
-          <span class="mb-3 mb-md-0 text-body-secondary">
-            &copy; 2024 Company, Inc
+          <span className="mb-3 mb-md-0 text-body-secondary">
+            &copy; Salah and Sem waren hier - sinds 2024
           </span>
         </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <li class="ms-3">
-            <a class="text-body-secondary" href="#">
-            <i class="fa-brands fa-twitter"></i>
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          <li className="ms-3">
+            <a className="text-body-secondary" href="#">
+            <i className="fa-brands fa-twitter"></i>
             </a>
           </li>
-          <li class="ms-3">
-            <a class="text-body-secondary" href="#">
-            <i class="fa-brands fa-instagram"></i>
+          <li className="ms-3">
+            <a className="text-body-secondary" href="#">
+            <i className="fa-brands fa-instagram"></i>
             </a>
           </li>
-          <li class="ms-3">
-            <a class="text-body-secondary" href="#">
-            <i class="fa-brands fa-facebook"></i>
+          <li className="ms-3">
+            <a className="text-body-secondary" href="#">
+            <i className="fa-brands fa-facebook"></i>
             </a>
           </li>
         </ul>
